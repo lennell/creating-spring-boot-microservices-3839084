de5f5494a30cf3ffc71082e3a545d180fc350ef0
SSSCKR <christer.lennell@scania.com>
2025-08-04 18:54:10 +0200

    a

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -1,16 +1,21 @@
 package com.example;
 
+import com.example.tourservice.TourManagementService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-import com.example.tourservice.TravelAgentService;
-
+@Configuration
+@ComponentScan(basePackages = "com.example.tourservice")
 public class Application {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService service = context.getBean(TourManagementService.class);
+        service.createTour("Zoo Tour", 100, true);
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
