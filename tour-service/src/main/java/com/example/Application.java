4c7dd91c97fddb750feb16709b14aed201608dce
SSSCKR <christer.lennell@scania.com>
2025-08-05 08:05:21 +0200

    a

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.tourservice.TourManagementService;
+import com.example.tourservice.TravelAgentService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
