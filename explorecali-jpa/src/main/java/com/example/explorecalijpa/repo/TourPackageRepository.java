aaed03413d919b18959482af8a4b6899e0698e2e
SSSCKR <christer.lennell@scania.com>
2025-08-05 09:37:52 +0200

    a

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,5 @@
+package com.example.explorecalijpa.repo;
+import com.example.explorecalijpa.model.TourPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface TourPackageRepository extends JpaRepository<TourPackage, String>{
+}
---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+import java.util.List;
+import com.example.explorecalijpa.model.Tour;
+import org.springframework.data.repository.CrudRepository;
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+
+}
