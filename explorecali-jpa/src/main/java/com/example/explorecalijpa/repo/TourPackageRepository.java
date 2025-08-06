1de9d7b5af3a1639d18a5d1aed99c7bc992b04a9
SSSCKR <christer.lennell@scania.com>
2025-08-06 12:49:21 +0200

    a

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -4,11 +4,14 @@ import com.example.explorecalijpa.model.TourPackage;
 
 import java.util.Optional;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /*
  * Challenge: Change url keyword to "packages"
  */
+@RepositoryRestResource (path = "packages", collectionResourceRel = "packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
   Optional<TourPackage> findByName(String name);
 }
